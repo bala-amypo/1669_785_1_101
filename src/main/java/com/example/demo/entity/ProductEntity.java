d6defe555e714155d3d11ea07d2ceb2a12ea1004
bala-amypo <bala@amypo.in>
2025-12-18 16:11:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/ProductEntity.java
+++ src/main/java/com/example/demo/entity/ProductEntity.java
@@ -2,7 +2,10 @@ package com.example.demo.entity;
 
 import java.util.L;
 import org.springframework.web.bind.annotation.Id;
-import jakarta.persistance.
+import jakarta.persistance.Id;
+import jakarta.persistance.GeneratedValue;
+import jakarta.persistance.GenerationType;
+
 import java.time.LocalDateTime;
 @Entity
 public class ProductEntity{
@@ -11,5 +14,10 @@ public class ProductEntity{
     Long id;
     private String warehouseName;
     private String location;
-    private LocalDateTime  
+    private LocalDateTime createdAt;
+
+    public Long getId(){
+        return id;
+    }
+    public void set
 }
