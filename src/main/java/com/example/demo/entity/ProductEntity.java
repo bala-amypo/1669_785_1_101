4121f6c22a4ab8ad3a4f0e45074bde04f81e2948
bala-amypo <bala@amypo.in>
2025-12-18 16:05:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/ProductEntity.java
+++ src/main/java/com/example/demo/entity/ProductEntity.java
@@ -1,4 +1,11 @@
 package com.example.demo.entity;
 
 @Entity
-public class ProductEntity
+public class ProductEntity{
+    @Id
+    @GerneratedValue(strategy=GenerationType.IDENTITY)
+    Long id;
+    private String warehouseName;
+    private String location;
+    private LocalDate 
+}
