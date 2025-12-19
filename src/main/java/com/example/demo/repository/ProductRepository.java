0e69aef3ed32e5bc78e883c53c9f33a0bcb0ecec
bala-amypo <bala@amypo.in>
2025-12-19 03:51:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,1 +1,7 @@
-package com.example.demo.repository
+package com.example.demo.repository;
+
+import com.example.demo.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Long>{
+
+}
