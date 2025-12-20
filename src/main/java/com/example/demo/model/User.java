d7d28075cd9b3df0c53cdeed41d04a7cfede74ac
bala-amypo <bala@amypo.in>
2025-12-20 05:30:28 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -55,11 +55,11 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(
-        name = "user_roles",
-        joinColumns = @JoinColumn(name = "user_id")
-    )
+    // @ElementCollection(fetch = FetchType.EAGER)
+    // @CollectionTable(
+    //     name = "user_roles",
+    //     joinColumns = @JoinColumn(name = "user_id")
+    // )
     // @Enumerated(EnumType.STRING)
     // @Column(name = "role", nullable = false)
     // private Set<Role> roles;
