b8d14e49e8813471c62ff243fb1d7a423bbff7ed
bala-amypo <bala@amypo.in>
2025-12-20 13:58:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,17 +2,22 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
-import com.example.demo.repository.ProductRepository;
 import java.util.List;
-@Service
-public class ProductServiceImpl implements ProductService{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+@RestController
+public class ProductController{
     @Autowired ProductService serve;
     @PostMapping("/api/products/POST")
-    Product createproduct(Product product){
+    Product createproduct(@RequestBody Product product){
         return serve.createProduct(product);
     }
     @GetMapping("/api/products/GET")
-    Product getproduct(Long id){
+    Product getproduct(@PathVariable Long id){
         return serve.getProduct(id);
     }
     @GetMapping("/api/products/GET")
--- src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.example.demo.service.ProductService;
 import com.example.demo.repository.ProductRepository;
 import java.util.List;
