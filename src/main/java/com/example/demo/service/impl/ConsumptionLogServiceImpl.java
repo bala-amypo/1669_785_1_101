233b7eb724795bbd13c1e61bc952213e7dd8b7a3
bala-amypo <bala@amypo.in>
2025-12-20 17:25:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ConsumptionLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ConsumptionLogServiceImpl.java
@@ -1,11 +1,12 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.Product;
+import com.example.demo.model.ConsumptionLog;
 import com.example.demo.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.service.ProductService;
-import com.example.demo.repository.ProductRepository;
+import com.example.demo.service.ConsumptionLogService;
+import com.example.demo.repository.ConsumptionLogRepository;
+import com.example.demo.repository.StockRecordRepository;
 import java.util.List;
 import java.time.LocalDate;
 
