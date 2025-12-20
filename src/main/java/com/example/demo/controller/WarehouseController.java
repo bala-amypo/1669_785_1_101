a80c0866759c08161368ad1c007b45abbd28b331
bala-amypo <bala@amypo.in>
2025-12-20 16:19:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/WarehouseController.java
+++ src/main/java/com/example/demo/controller/WarehouseController.java
@@ -1,7 +1,13 @@
+package com.example.demo.controller
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
-import java.util.*;
-
+import com.example.demo.service.WarehouseService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/warehouses")
