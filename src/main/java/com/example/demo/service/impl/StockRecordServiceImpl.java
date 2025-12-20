b23227cacca00aac30a543beb43b76462d9fd215
bala-amypo <bala@amypo.in>
2025-12-20 17:40:48 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StockRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StockRecordServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.StockRecord;
+import com.example.demo.model.Product;
+import com.example.demo.model.Warehouse;
 import com.example.demo.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
