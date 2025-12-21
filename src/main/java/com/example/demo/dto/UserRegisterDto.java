1cabf1fce975076e5b4477da8c18e5b3019fed92
bala-amypo <bala@amypo.in>
2025-12-21 05:12:28 +0000

    User commit

--- src/main/java/com/example/demo/dto/UserRegisterDto.java
+++ src/main/java/com/example/demo/dto/UserRegisterDto.java
@@ -1,5 +1,10 @@
 package com.example.demo.dto;
 
 public class UserRegisterDto{
-    
+    public String getEmail(){
+        return null;
+    }
+    public String getPassword(){
+        return null;
+    }
 }
