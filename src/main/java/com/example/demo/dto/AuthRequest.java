3a99d7488aaf55735fec48b4f571da5eb7ebf237
bala-amypo <bala@amypo.in>
2025-12-21 05:18:28 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,5 +1,7 @@
 package com.example.demo.dto;
 
 public class AuthRequest{
-    
+    public AuthRequest(String msg){
+        
+    }
 }
--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,5 +1,7 @@
 package com.example.demo.dto;
 
 public class AuthResponse{
-    
+    public AuthResponse(String msg){
+        
+    }
 }
