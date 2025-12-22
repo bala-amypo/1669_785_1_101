82ad0ea8ad007928d68d6683ec2d0017ccd47bdb
bala-amypo <bala@amypo.in>
2025-12-22 05:36:58 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,27 +1,27 @@
-package com.example.demo.exception;
+// package com.example.demo.exception;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+// import java.time.LocalDateTime;
+// import java.util.HashMap;
+// import java.util.Map;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.ControllerAdvice;
+// import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
+// @ControllerAdvice
+// public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(
-            ResourceNotFoundException ex) {
+//     @ExceptionHandler(ResourceNotFoundException.class)
+//     public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(
+//             ResourceNotFoundException ex) {
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("timestamp", LocalDateTime.now());
-        response.put("status", HttpStatus.NOT_FOUND.value());
-        response.put("error", "Not Found");
-        response.put("message", ex.getMessage());
+//         Map<String, Object> response = new HashMap<>();
+//         response.put("timestamp", LocalDateTime.now());
+//         response.put("status", HttpStatus.NOT_FOUND.value());
+//         response.put("error", "Not Found");
+//         response.put("message", ex.getMessage());
 
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-}
+//         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+//     }
+// }
