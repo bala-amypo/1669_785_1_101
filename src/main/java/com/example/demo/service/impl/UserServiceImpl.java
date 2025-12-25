9b8cb46353ee2bce1bb1c5acc03d0eb4e9fd062d
bala-amypo <bala@amypo.in>
2025-12-25 17:29:49 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -39,7 +39,7 @@ public AuthResponse login(AuthRequest request) {
         throw new IllegalArgumentException("Invalid email or password");
     }
     AuthResponse response = new AuthResponse();
-response.setToken(""dummy-jwt-token"");
+response.setToken("dummy-jwt-token");
 return response;
 }
 
