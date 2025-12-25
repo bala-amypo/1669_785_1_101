8a80d8a49399e2042c8417e351ff24a09b2b07e9
bala-amypo <bala@amypo.in>
2025-12-25 17:08:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,8 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
-import jakarta.validation.Valid; 
+
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.UserRegisterDto;
@@ -11,26 +10,20 @@ import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 
 @RestController
-@RequestMapping("/auth")
+// @RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/register")
-    // public User register(@RequestBody UserRegisterDto dto) {
-    //     return userService.register(dto);
-    // }
-public ResponseEntity<String> register(
-        @Valid @RequestBody UserRegisterDto request) {
+    @PostMapping("/auth/register")
+    public User register(@RequestBody UserRegisterDto dto) {
+        return userService.register(dto);
+    }
 
-    userService.register(request);
-    return ResponseEntity.ok("User registered successfully");
-}
-
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
         return userService.login(request);
     }
-   
+    
 }
