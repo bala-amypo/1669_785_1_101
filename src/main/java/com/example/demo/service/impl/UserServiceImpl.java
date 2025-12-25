b7d73ce7e2588461dc589bbaebf092476256b75e
bala-amypo <bala@amypo.in>
2025-12-25 17:10:10 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -29,9 +29,19 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public AuthResponse login(AuthRequest request) {
-        return new AuthResponse("dummy-jwt-token");
+public AuthResponse login(AuthRequest request) {
+
+    User user = userRepository.findByEmail(request.getEmail())
+            .orElseThrow(() ->
+                new IllegalArgumentException("Invalid email or password"));
+
+    if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+        throw new IllegalArgumentException("Invalid email or password");
     }
+
+    String token = jwtService.generateToken(user);
+    return new AuthResponse(token);
+}
 
     @Override
     public User getByEmail(String email) {
