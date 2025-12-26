a87ac44774175ccd42ed49864a44bf04c89d6c65
bala-amypo <bala@amypo.in>
2025-12-26 09:07:21 +0000

    User commit

--- src/main/java/com/example/demo/dto/UserRegisterDto.java
+++ src/main/java/com/example/demo/dto/UserRegisterDto.java
@@ -7,7 +7,7 @@ import lombok.Data;
 public class UserRegisterDto{
     @NotBlank(message = "Username is required")
     private String username;
-
+    
     @NotBlank(message = "Password is required")
     private String password;
 
