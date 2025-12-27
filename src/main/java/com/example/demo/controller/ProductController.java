6566218921ae536ec641a18ad28f9ac26637844e
bala-amypo <bala@amypo.in>
2025-12-27 08:37:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,39 +2,33 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/products")
-@Tag(name = "Product")
 public class ProductController {
 
-    @Autowired private ProductService productService;
+    private final ProductService productService;
 
-// public ProductController(ProductService productService) {
-// this.productService = productService;
-// }
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
-@PostMapping
-public Product create(@RequestBody Product product) {
-return productService.createProduct(product);
-}
+    @PostMapping
+    public Product create(@Valid @RequestBody Product product) {
+        return productService.createProduct(product);
+    }
 
-@GetMapping
-public List<Product> getAll() {
-return productService.getAllProducts();
-}
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        return productService.getProduct(id);
+    }
 
-@GetMapping("/{id}")
-public Product get(@PathVariable Long id) {
-return productService.getProduct(id);
-}
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
 }
