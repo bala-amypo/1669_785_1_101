a4704116fda3ef446ed42dc8fe791032c889f0e7
bala-amypo <bala@amypo.in>
2025-12-27 05:56:17 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -1,5 +1,31 @@
-package com.example.demo.config;
+package com.example.demo.security;
 
-public class JwtAuthenticationFilter{
-    
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter implements Filter {
+
+    private final JwtTokenProvider provider;
+    private final CustomUserDetailsService uds;
+
+    public JwtAuthenticationFilter(JwtTokenProvider provider,
+                                   CustomUserDetailsService uds) {
+        this.provider = provider;
+        this.uds = uds;
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            provider.validateToken(header.substring(7));
+        }
+        chain.doFilter(req, res);
+    }
 }
