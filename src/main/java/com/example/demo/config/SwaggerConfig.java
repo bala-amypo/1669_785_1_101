f71c439a98c0630f8bedc5e6d1f2828596ac3c62
bala-amypo <bala@amypo.in>
2025-12-28 16:21:29 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
 
                 // Server Configuration
                 .servers(List.of(
-                        new Server().url("https://9107.pro604cr.amypo.ai")
+                        new Server().url("https://9194.pro604cr.amypo.ai/")
                 ))
 
                 // Security Configuration (JWT Bearer)
