79ef81f5501c0389d172a47199f7fe0df0232a2a
bala-amypo <bala@amypo.in>
2025-12-28 07:23:08 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
-import com.example.demo.config;
+
+import com.example.demo.config.JwtAuthenticationFilter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
